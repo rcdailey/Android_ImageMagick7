874ff46cafe952cafcd5fd57f75a47757c2501ea
Robert Dailey <rcdailey@gmail.com>
2017-05-22 08:17:35 -0500

    Update Application.mk for API15, Clang, shared gnustl, etc.
    These changes are ad-hoc and are for $DAYJOB. I hard-coded the changes
    because I'm lazy and I'm not going to use this repo for anything else.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,10 +1,13 @@
-# list of supported architectures 
-# https://developer.android.com/ndk/guides/application_mk.html
-APP_ABI := all 
-#APP_ABI := armeabi-v7a
-APP_PLATFORM := android-12
-NDK_TOOLCHAIN_VERSION := 4.9
-# list of build-in STL's supported by NDK. 
-# switch to clang STL in case of using this compiler
-# https://developer.android.com/ndk/guides/cpp-support.html#runtimes
-APP_STL := stlport_static
+APP_ABI := armeabi-v7a x86
+APP_PLATFORM := android-15
+APP_STL := gnustl_shared
+APP_MODULES := libjpeg libtiff libpng MagickCore-7 MagickWand-7
+NDK_TOOLCHAIN_VERSION := clang
+
+APP_CFLAGS += \
+    -DMAGICKCORE_HDRI_ENABLE=0 \
+    -DMAGICKCORE_QUANTUM_DEPTH=8 \
+
+APP_CPPFLAGS += \
+    -DMAGICKCORE_HDRI_ENABLE=0 \
+    -DMAGICKCORE_QUANTUM_DEPTH=8 \
